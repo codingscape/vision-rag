e20618fa04e3b46b6496621976f6982de2793d01
Jaime Burnap <jaime@burnap.io>
2024-09-23 11:10:57 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Codingscape Vision RAG Image Generator
+This application uses real life imagery to generate images that match the user's description.
+
+## How it works
+1. Find places using Google's Place API
+2. Send coordinates from step 1 to Google Street view and get images from 0, 90, 180, and 270 degrees for up to 4 of the places
+3. Send those images to llava to generate descriptions of the images
+4. Condense all descriptions into a single description using llava
+5. Use condensed description as a prompt to stable diffusion
+
+## Model versions
+- llava-hf/llava-v1.6-mistral-7b-hf
+- stabilityai/stable-diffusion-3-medium-diffusers
+
+# Running
+
+## Deployment
+SSH is the only supported way of running this on anything that's not local. `scp` `main.py` and `requirements.txt` to your chosen environment.
+
+## Application
+Pytorch must be installed separately due to varying hardware differences. Once that is installed, run `pip install -r requirements.txt`
+
+Right now, it's a standalone script with the prompt hard coded into it. Edit the prompt and run `python main.py`.
+
+You will need some pretty beefy hardware to run this on unless you have access to RunPod or GPU instances in AWS.
---
+++ main.py
@@ -0,0 +1,223 @@
+import random
+import requests
+import torch
+from diffusers import StableDiffusion3Pipeline
+import googlemaps
+import os
+from PIL import Image
+from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration, T5EncoderModel, BitsAndBytesConfig # Added T5EncoderModel & BitsAndBytesConfig
+import re
+
+HF_API_KEY = ""
+GOOGLE_API_KEY = ""
+HEADINGS = [0, 90, 180, 270]
+IMAGE_FOLDER = "images"
+
+# llava quantization config
+llava_quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16,
+)
+# sd quantization config
+sd_quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+
+
+def get_street_view_image(coords):
+    print("Fetching Street View images")
+    
+    if not os.path.exists(IMAGE_FOLDER):
+        os.makedirs(IMAGE_FOLDER)
+
+    for idx, coord in enumerate(coords):
+        for heading in HEADINGS:
+            url = f"https://maps.googleapis.com/maps/api/streetview?size=600x300&location={coord[0]},{coord[1]}&heading={heading}&key={GOOGLE_API_KEY}"
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                image_path = os.path.join(IMAGE_FOLDER, f'street_view_heading_{idx}_{heading}.jpg')
+                with open(image_path, 'wb') as f:
+                    f.write(response.content)
+                print(f"Image saved at {image_path}")
+            else:
+                print(f"Error fetching image for heading {heading}. Status code: {response.status_code}")
+
+    print("Street View images fetched")
+
+def get_location_coordinates(description):
+    print("Getting location coordinates")
+    gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
+
+    result = gmaps.places(description, "textquery")
+
+    size = len(result['results'])
+
+    rand = random.sample(result['results'], int(size / 5))
+
+    coords = []
+
+    for r in rand:
+        coords.append((r['geometry']['location']['lat'], r['geometry']['location']['lng']))
+
+    print("Got location coordinates")
+
+    return coords
+
+def generate_image_descriptions():
+    print("Generating image descriptions")
+
+    processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
+
+    model = LlavaNextForConditionalGeneration.from_pretrained(
+        "llava-hf/llava-v1.6-mistral-7b-hf",
+        torch_dtype=torch.float16,
+        quantization_config=llava_quantization_config, # set quant
+        device_map="cuda:0", # inline set device
+        attn_implementation="flash_attention_2", # flash attn 2
+    )
+    # model.to("cuda:0")
+
+    descriptions = []
+    images = []
+
+    for image_file in os.listdir(IMAGE_FOLDER):
+        if image_file.endswith('.jpg'):
+            image_path = os.path.join(IMAGE_FOLDER, image_file)
+            image = Image.open(image_path)
+
+            images.append(image)
+
+    conversation = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text",
+                 "text": "What is shown in each image? I don't care or want to know about watermarks. Keep it concise. Especially pay attention to weather, architecture, and lighting."},
+                {"type": "image"},
+            ],
+        },
+    ]
+
+    prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
+    inputs = processor(images=images, text=[prompt], return_tensors="pt").to("cuda:0")
+
+    response = model.generate(**inputs, max_new_tokens=4096)
+
+    decoded = processor.decode(response[0], skip_special_tokens=True)
+    cleaned = re.sub(r'\[INST\][\w\s\d\?]*.*\[\/INST\]', '', decoded, flags=re.MULTILINE).strip()
+
+    # descriptions.append(cleaned)
+    #
+    # combined_description = " ".join(descriptions)
+
+    print("Image descriptions generated")
+
+    print(cleaned)
+
+    return cleaned
+
+def combine_descriptions(descriptions):
+    print("Combining descriptions")
+
+    combined_prompt = "Summarize the following descriptions, try to keep specific things about weather, architecture, and lighting: " + " ".join(descriptions)
+
+    processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
+    model = LlavaNextForConditionalGeneration.from_pretrained(
+        "llava-hf/llava-v1.6-mistral-7b-hf",
+        torch_dtype=torch.float16,
+        quantization_config=llava_quantization_config, # set quant
+        device_map="cuda:0", # inline set device
+        attn_implementation="flash_attention_2", # flash attn 2
+    )
+    # model.to("cuda:0")
+
+    conversation = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": combined_prompt}
+            ],
+        },
+    ]
+
+    prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
+    inputs = processor(text=prompt, return_tensors="pt").to("cuda:0")
+
+    response = model.generate(**inputs, max_new_tokens=400)
+
+    decoded = processor.decode(response[0], skip_special_tokens=True)
+    summary_description = re.sub(r'\[INST\][\w\s\d\?]*.*\[\/INST\]', '', decoded, flags=re.MULTILINE).strip()
+
+    print("Descriptions combined")
+
+    return summary_description
+
+def create_optimized_prompt(llava_description):
+    optimized_prompt = f"The location looks like this: {llava_description}"
+    return optimized_prompt
+
+def generate_final_image(prompt):
+    print("Generating final image")
+    #pipe = StableDiffusion3Pipeline.from_pretrained(
+    #    "stabilityai/stable-diffusion-3-medium-diffusers",
+    #    torch_dtype=torch.float16,
+    #    negative_prompt="",
+    #    num_inference_steps=28,
+    #    guidance_scale=7.0,
+    #)
+    #pipe = pipe.to("cuda")
+
+    model_id = "stabilityai/stable-diffusion-3-medium-diffusers"
+    text_encoder = T5EncoderModel.from_pretrained(
+        model_id,
+        subfolder="text_encoder_3",
+        quantization_config=sd_quantization_config,
+    )
+    pipe = StableDiffusion3Pipeline.from_pretrained(
+        model_id,
+        text_encoder_3=text_encoder,
+        device_map="balanced",
+        torch_dtype=torch.float16
+    )
+
+    
+    image = pipe(prompt).images[0]
+    
+    image.save("final_image.png")
+    print("Final image generated.")
+
+
+def main():
+    # Step 1: Fetch Street View image
+    coords = get_location_coordinates(user_prompt)
+    get_street_view_image(coords)
+
+    print()
+    print()
+
+    # Step 2: Get image description from LLaVA
+    image_descriptions = generate_image_descriptions()
+    print(f"Generated description: {image_descriptions}")
+
+    print()
+    print()
+
+    # Step 3: Combine descriptions
+    summary = combine_descriptions(image_descriptions)
+    print(summary)
+
+    print()
+    print()
+
+    # Step 4: Create optimized prompt
+    optimized_prompt = create_optimized_prompt(summary)
+    # print(f"Optimized prompt: {optimized_prompt}")
+
+    print()
+    print()
+
+    # Step 5: Generate final image using SDXL-Turbo
+    generate_final_image(optimized_prompt)
+
+user_prompt = "paris, france cafe"
+main()
